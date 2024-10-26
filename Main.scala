a2059cca9a9a9df561a7bea394184f5be803f377
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-10-26 12:51:32 +0000

    dotenv-java を削除

--- Main.scala
+++ Main.scala
@@ -1,32 +1,28 @@
 //> using dep "com.lihaoyi::upickle:4.0.2"
-//> using dep "io.github.cdimascio:dotenv-java:3.0.2"
 //> using dep "it.sauronsoftware.cron4j:cron4j:2.2.5"
 //> using dep com.softwaremill.sttp.client4::core:4.0.0-M6
 
-import io.github.cdimascio.dotenv.Dotenv
 import it.sauronsoftware.cron4j.Scheduler
 import java.time.format.DateTimeFormatter.ISO_INSTANT
 import java.time.ZonedDateTime
 import upickle.default.{ReadWriter, read}
 
 @main def Main() = {
+  val SERVICE = sys.env.getOrElse("SERVICE", "https://bsky.social/")
+  val IDENTIFIER = sys.env.getOrElse("IDENTIFIER", "")
+  val PASSWORD = sys.env.getOrElse("PASSWORD", "")
+
   val scheduler = new Scheduler()
 
   scheduler.schedule(
     "0 4 * * *",
     () => {
-      val dotenv = Dotenv.load()
 
-      val agent: Agent = Agent(service = dotenv.get("SERVICE"))
-      agent.createSession(
-        identifier = dotenv.get("IDENTIFIER"),
-        password = dotenv.get("PASSWORD")
-      )
+      val agent: Agent = Agent(service = SERVICE)
 
-      val authorFeed: String = agent.getAuthorFeed(
-        limit = 100,
-        cursor = ZonedDateTime.now().minusMonths(1).format(ISO_INSTANT).toString()
-      )
+      agent.createSession(identifier = IDENTIFIER, password = PASSWORD)
+
+      val authorFeed: String = agent.getAuthorFeed(limit = 100, cursor = ZonedDateTime.now().minusMonths(1).format(ISO_INSTANT).toString())
 
       case class Viewer(repost: Option[String] = None) derives ReadWriter
       case class Post(uri: String, viewer: Viewer) derives ReadWriter
@@ -45,5 +41,4 @@ import upickle.default.{ReadWriter, read}
   )
 
   scheduler.start()
-
 }
